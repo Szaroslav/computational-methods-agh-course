6efd035ed027936c656995357b6d59f8be7ec03b
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-17 18:39:26 +0200

    Init `React app`

---
+++ lab06/app/src/components/search/Search.js
@@ -0,0 +1,15 @@
+import './Search.css';
+
+function Search() {
+    return (
+        <div>
+            <h1>Chess search</h1>
+            <form>
+                <input type="search" />
+                <input type="button" value="Search" />
+            </form>
+        </div>
+    );
+}
+
+export default Search;
