74e53fe493c531f9f9517a1a177b77cdc89d77a1
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-17 22:15:05 +0200

    Refactor `init.py`, split `init()` into smaller chunks of code

--- lab06/core/db/init.py
+++ lab06/core/db/init.py
@@ -12,7 +12,6 @@ import numpy as np
 
 
 connection = redis.Redis(host="localhost", port=6379, decode_responses=True)
-print(connection.ping())
 
 STOP_WORDS = [
     "a",
@@ -96,6 +95,47 @@ STOP_WORDS = [
     "your"
 ]
 
+# nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+
+
+def frequency_vector(words: list[str]) -> dict[int]:
+    frequency_vector: dict[int] = {}
+
+    for word in words:
+        word = stemmer.stem(word)
+        if frequency_vector.get(word) is None:
+            frequency_vector[word] = 0
+        frequency_vector[word] += 1
+    
+    return frequency_vector
+
+
+def sparse_matrix(vector: dict[int], words_dict: dict[int], j: int) -> tuple[list[int], list[int], list[float]]:
+    sparse_matrix: tuple[list[int], list[int], list[float]] = ([], [], [])
+    rows, cols, values = sparse_matrix
+
+    for key in vector:
+        if words_dict.get(key) is not None:
+            rows.append(words_dict[key])
+            cols.append(j)
+            values.append(vector.get(key))
+
+    return sparse_matrix
+
+
+def document_frequency(vector: dict[int], words_dict: dict[int]) -> np.ndarray:
+    doc_frequency = np.zeros(len(words_dict))
+
+    for key in vector:
+        if words_dict.get(key) is not None:
+            doc_frequency[words_dict[key]] += 1
+
+    return doc_frequency
+
 
 def init():
     CONTENT_TAG_NAME = "text"
@@ -108,16 +148,12 @@ def init():
     m: int = 0
     mx, mn = (0, ""), (math.inf, "")
     words_dict: dict[str, int] = {}
-    words_base: list[str] = []
 
-    # nltk.download('wordnet')
-    # nltk.download('punkt')
-    # nltk.download('averaged_perceptron_tagger')
-    # lemmatizer = WordNetLemmatizer()
-    stemmer = PorterStemmer()
+    # for word in tokenize("chess is an abstract board game. playing chess is really fun. king is a chess piece"):
+    #     print(stemmer.stem(word))
 
     def idf(i: int):
-        return np.log10(n / A.getrow(i).getnnz(1))
+        return np.log10(n / doc_f[i])
 
     def get_poses(words: list[str]):
         tag_dict = {
@@ -167,30 +203,21 @@ def init():
     #
     print("Iterating over every document available in the dumps...")
     rows, cols, values = [], [], []
+    doc_f = None
     for filename in filenames:
         with open(filename, "r") as file:
             parser = wp.WikiParser(file)
 
             while (doc := parser.parse_document()) is not None:
-                di = {}
                 words = tokenize(clean(doc).lower())
-
-                for word in words:
-                    word = stemmer.stem(word)
-                    if di.get(word) is None:
-                        di[word] = 0
-                    di[word] += 1
-                
-                for key in di:
-                    if words_dict.get(key) is not None:
-                        rows.append(words_dict[key])
-                        cols.append(n)
-                        values.append(di.get(key))
+                di = frequency_vector(words)
+                rows, cols, values = sparse_matrix(di, words_dict, n)
+                doc_f = document_frequency(di, words_dict)
 
                 n += 1
                 # print(n)
 
-    # A = coo_matrix((values, (row, col)), shape=(m, n))
+    # A = coo_matrix((values, (rows, cols)), shape=(m, n))
     # IDF = np.empty(m)
     # for i in range(m):
     #     IDF[i] = idf(i)
@@ -199,10 +226,17 @@ def init():
     # for i, j in indicies:
     #     A[i, j] *= IDF[i]
 
+    IDF = np.empty(m)
+    for i in range(m):
+        IDF[i] = idf(i)
+
+    for k, i in enumerate(rows):
+        values[k] *= IDF[i]
+
     print(len(rows))
 
-    with open("dt-sparse.min.json", "w") as file:
-        file.write(json.dumps([{ "row": r, "col": c, "value": v } for r, c, v in zip(rows, cols, values)]))
+    with open("dt-sparse.json", "w") as file:
+        file.write(json.dumps([{ "row": r, "col": c, "value": v } for r, c, v in zip(rows, cols, values)], indent=4))
 
     print(f"{m}, {n}")
 
